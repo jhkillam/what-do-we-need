69fa7880152a14d808ca0dd38e36b074993afeee
Joseph Killam <jhkillam@gmail.com>
2020-08-29 16:26:52 -0500

    created add-item component

--- client/src/app/components/add-item/add-item.component.ts
+++ client/src/app/components/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from 'src/app/services/item.service'
 
 @Component({
   selector: 'app-add-item',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
+  item = {
+    itemName: '',
+    store: '',
+    purchased: false
+  }
+  submitted = false
 
-  constructor() { }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
   }
 
+  saveItem(): void {
+    const data = {
+      itemName: this.item.itemName,
+      store: this.item.store
+    }
+
+    this.itemService.create(data)
+      .subscribe(
+        response => {
+          console.log(response)
+          this.submitted = true
+        },
+        error => {
+          console.log(error)
+        })
+  }
+
+  newItem(): void {
+    this.submitted = false
+    this.item = {
+      itemName: '',
+      store: '',
+      purchased: false
+    }
+  }
 }
